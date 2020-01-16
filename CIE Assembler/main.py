83b21bc049541c5b3c75eaa3e87053393cb61415
Kyouma-BAT <kyouma.bat@gmail.com>
2020-01-16 20:28:24 +0000

    Added Some Toolbar code

--- CIE Assembler/main.py
+++ CIE Assembler/main.py
@@ -2,12 +2,16 @@ try:
     from Tkinter import *
 except:
     from tkinter import *
+from toolbar import *
 
 
 if __name__ == "__main__":
     root = Tk()
     root.title("CIE Assembler virtual machine")
-    
+    root.geometry("600x600")
+
+    toolBar = ToolBar(root)
+
 
 
 
--- CIE Assembler/toolbar.py
+++ CIE Assembler/toolbar.py
@@ -1,1 +1,22 @@
-#There will be code here
+try:
+    from Tkinter import *
+except:
+    from tkinter import *
+from collections import OrderedDict
+
+class ToolBar:
+    def __init__(self, master):
+        self.master = master
+        self.toolbar = Menu(self.master)
+        self.bars = OrderedDict()
+        self.bars["File"] = Menu(self.toolbar)
+        self.bars["Tools"] = Menu(self.toolbar)
+        self.bars["Config"] = Menu(self.toolbar)
+        self.bars["Help"] = Menu(self.toolbar)
+        self.bars["About"] = Menu(self.toolbar)
+
+        for each in self.bars:
+            self.toolbar.add_cascade(label = each, menu = self.bars[each])
+        self.master.config(menu = self.toolbar)
+
+        self.bars["File"].add_command(label = "Save")
