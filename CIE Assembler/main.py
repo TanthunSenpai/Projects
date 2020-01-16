cbb2111a6c06d8fed3288e2f79b27b8a026b037f
Kyouma-BAT <kyouma.bat@gmail.com>
2020-01-16 20:22:50 +0000

    File Structure Commit.

--- CIE Assembler/main.py
+++ CIE Assembler/main.py
@@ -1,1 +1,16 @@
-#Python n00bie
+try:
+    from Tkinter import *
+except:
+    from tkinter import *
+
+
+if __name__ == "__main__":
+    root = Tk()
+    root.title("CIE Assembler virtual machine")
+    
+
+
+
+
+
+    root.mainloop()
