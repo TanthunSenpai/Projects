310c61d02d1be1d50255da03242a42ba2bebb64e
Laveen <laveen2512@gmail.com>
2020-01-23 20:30:49 +0000

    Initial code

--- CIE Assembler/assembler.py
+++ CIE Assembler/assembler.py
@@ -1,1 +1,9 @@
-#there will be code here
+class assembly:
+    def __init__(self):
+        self.registerDict = { #Dictionary to hold all registers
+            "ACC":None,
+            "IX":None,
+            "PC":None,
+            "SP":None,
+            "Z":None
+        }
---
+++ CIE Assembler/syntax.py
@@ -0,0 +1,37 @@
+OPCODETOHEXDICT = { #CONSTANT Dictionary to hold keywords and functions that will be called
+    "LDM":"0A",
+    "LDD":"0B",
+    "LDI":"0C",
+    "LDX":"0D",
+    "LDR":"0E",
+    "STO":"0F",
+    "ADD":"1A",
+    "INC":"1B",
+    "DEC":"1C",
+    "JMP":"1D",
+    "CMP":"1E",
+    "JPE":"1F",
+    "JPN":"2A",
+    "IN":"2B",
+    "OUT":"2C",
+    "END":"2D"
+}
+
+HEXTOFUNCTIONDICT = {
+    "0A":LDM,
+    "0B":LDD,
+    "0C":LDI,
+    "0D":LDX,
+    "0E":LDR,
+    "0F":STO,
+    "1A":ADD,
+    "1B":INC,
+    "1C":DEC,
+    "1D":JMP,
+    "1E":CMP,
+    "1F":JPE,
+    "2A":JPN,
+    "2B":IN,
+    "2C":OUT,
+    "2D":END
+}
