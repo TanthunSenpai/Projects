f3e44182919523d5397e929e91d6afa9f2b738a8
Kyouma-BAT <kyouma.bat@gmail.com>
2020-03-05 20:26:43 +0000

    *Error Line created *Syntax checks for the length of 3 characters *Error messages eeeee

--- CIE Assembler/editor.py
+++ CIE Assembler/editor.py
@@ -4,6 +4,31 @@ except:
     from Tkinter import *
 
 import syntax
+import random
+
+cheerMessages = ["Amazing! you have no errors, I am surprised",
+                "Great!",
+                "FINALLY",
+                "It took you a long time, but you did it",
+                "I didn't know you are capable of writing errorless code",
+                "Good Job!",
+                "AHahahahhahaha",
+                "I doubt you understood what you did, but anyway Good Job!",
+                "Nice!",
+                "Is it bird!?.. Is it an airplane!?... NO! it's a code without mistakes..."
+                ]
+
+
+def check_syntax(token_list):
+    for i,line in enumerate(token_list):
+        if len(line) > 3:
+            return False, i+1, "too many words in a line"
+
+
+
+    return True, "", random.choice(cheerMessages)
+
+
 
 
 def is_valid_operand(s):
@@ -22,43 +47,16 @@ class Editor:
         self.frame.grid(row=r, column=c)
         self.scrollBar = Scrollbar(self.frame, orient=VERTICAL)
         self.scrollBar.pack(side=RIGHT, fill=Y)
-        self.textArea = Text(self.frame, width=40, height=20,
-                             font=self.font, yscrollcommand=self.scrollBar.set)
+        self.textArea = Text(self.frame,
+                            width=40,
+                            height=25,
+                            font=self.font,
+                            yscrollcommand=self.scrollBar.set
+                            )
         self.textArea.pack(side=LEFT)
         self.scrollBar.config(command=self.textArea.yview)
         self.textArea.insert(INSERT, "Write your text here my friend...")
 
-    def check_syntax(self, text):
-        ret = True
-        for i,line in enumerate(text):
-            if len(line) > 3:
-                ret = str(i) + " LINE TOO LONG"
-            else:
-                if len(line) == 1:
-                    if line[0] != "END":
-                        ret = str(i) + " IT HAS TO BE END"
-
-                elif len(line) == 2:
-                    if not(line[0] in syntax.OPCODETOHEXDICT):
-                        ret = str(i) + " NOT A VALID OPCODE"
-
-                    if not(is_valid_operand(line[1])):
-                        ret = str(i) + " NOT A VALID OPERAND"
-
-                elif len(line) == 3:
-                    if line[0][-1] != ':':
-                        print(line[0][-1])
-                        print(':')
-                        ret = str(i) + " YOU NEED :"
-                    if not(line[1] in syntax.OPCODETOHEXDICT):
-                        ret = str(i) + " NOT A VALID OPCODE"
-                    if not(is_valid_operand(line[2])):
-                        ret = str(i) + " NOT A VALID OPERAND"
-        return ret
-
-
-
-        return 1
 
     def lexical_analysis(self, *arg):
         text = self.textArea.get('0.0', 'end').strip()
@@ -67,8 +65,12 @@ class Editor:
 
         for line in text:
             ret.append(line.strip().split())
-        print(ret)
-        print(self.check_syntax(ret))
+        is_valid, linNo, msg = check_syntax(ret)
+        if not is_valid:
+            self.rep("Oopsie on line:" + str(linNo) + "|errorCode: " + msg)
+        else:
+            self.rep("No error: " + msg)
+
 
         return ret
 
--- CIE Assembler/errorLine.py
+++ CIE Assembler/errorLine.py
@@ -1,1 +1,37 @@
-#There will be code here
+try:
+    from Tkinter import *
+except:
+    from tkinter import *
+
+
+class ErrorBar:
+    def __init__(self, master, r, c):
+        self.master = master
+        self.frame = Frame(master)
+        self.frame.grid(row = r, column = c, sticky = S)
+        self.fontSize = 12
+        self.font = ("Consolas", self.fontSize)
+        self.text = "Don't worry my friend, you have no errors....... yet"
+        self.textBar = Label(self.frame,
+                            text = self.text,
+                            font = self.font,
+                            fg = "black",
+                            width = 50,
+                            justify = LEFT,
+                            wraplength = 550
+                            )
+        self.textBar.grid(row = 0, column = 0, sticky = S)
+
+    def update(self, errMsg, *args):
+        self.textBar.configure(text = errMsg)
+        self.master.update()
+
+
+
+if __name__ == "__main__":
+    root = Tk()
+    bar = ErrorBar(root, 0, 0)
+
+
+
+    root.mainloop()
