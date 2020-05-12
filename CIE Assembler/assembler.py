a086a8ebd051884c5839b6454901556a745cdef0
Laveen <laveen2512@gmail.com>
2020-05-12 17:20:54 +0100

    Test

--- CIE Assembler/assembler.py
+++ CIE Assembler/assembler.py
@@ -6,9 +6,9 @@
 
 #Update: Can do everything in 1 pass, so no longer using 2 pass model
 
-#from syntax import * #Importing the dictionaries declared in syntax.py to be used later (may be useful, not sure yet)
+import syntax #Importing the dictionaries declared in syntax.py to be used later (may be useful, not sure yet)
 
-class Assembler:
+class Assembler: #Writing as a class so we can have a separate class for each assembly code file.
     def __init__(self):
         self.symbolTable = [] #Structure of the symbol table will be [[label name, address in memory location]]
         self.RAM = [] #Initalises RAM by default on constructor call
@@ -23,7 +23,7 @@ class Assembler:
             self.RAM.append("00") #Initialising each memory location to 00 in hex
         return self.RAM #Return the initialised RAM list
 
-    def showContents(self): #TEST FUNCTION TO BE REMOVED LATER
+    def showContents(self): #TEST FUNCTION TO BE CALLED FOR DEBUG PURPOSES
         print("Current RAM state: ")
         print(self.RAM)
         print("\nSymbol Table: ")
@@ -33,25 +33,38 @@ class Assembler:
         #The line passed in will be a label, followed by an opcode and an operand
         #For label case, we just place the label and it's location in memory into the symbol table
         #The rest of the label will be a regular opcode case, and so we can just add these into RAM
+        label = line.pop(0)
+        if label in self.symbolTable: #Means we tried jumping to it before it was defined
+
         self.symbolTable.append([]) #Appends a row to the symbol table so we can add our label and position in RAM
-        self.symbolTable[len(self.symbolTable) - 1].append(line[0])
-        hexLine = hex(self.RAMpointer)
+        self.symbolTable[len(self.symbolTable) - 1].append(line)
+        hexLine = hex(self.RAMpointer) #Converts pointer's current position into hex
         hexLine = hexLine[2:] #Removes the 0x at the beginning of the hex number
-        self.symbolTable[len(self.symbolTable) - 1].append(hexLine)
-        self.RAM[self.RAMpointer] = line[1] #Opcode
+        self.symbolTable[len(self.symbolTable) - 1].append(hexLine) #Adds label start position in RAM to the symbol table
+        if len(line) == 1:
+            self.specialOpcode(line)
+        else:
+            self.regularOpcode(line)
+
+    def regularOpcode(self, line): #Deals with adding OPCODE OPERAND lines to RAM
+        self.RAM[self.RAMpointer] = syntax.OPCODETOHEXDICT[line[0]] # First part will always be an opcode
         self.RAMpointer += 1
-        self.RAM[self.RAMpointer] = line[2] #Operand
+        if line[1] in syntax.SPECIALOPERANDS: #Checking if the original line was in the form OPCODE SPECIALOPERAND
+            self.RAM[self.RAMpointer] = syntax.SPECIALOPERANDS[line[1]]
+        elif line[0] == "JMP": #Means the OPERAND is either an address number or a label
+            try:
+                operand = int(line[1])
+            except: #If the above failed, it must be a label operand
+
+        else:
+            self.RAM[self.RAMpointer] = hex(int(line[1]))[2:].upper() #Operand must be a number if we reached this point
+            if len(self.RAM[self.RAMpointer]) == 1: #Means we should add an extra 0 at the beginning
+                self.RAM[self.RAMpointer] = "0" + self.RAM[self.RAMpointer]
         self.RAMpointer += 1
 
-
-    def regularOpcode(self, line):
-        self.RAM[self.RAMpointer] = line[1] #Opcode
+    def specialOpcode(self, line): #Deals with adding OPCODE lines to RAM
+        self.RAM[self.RAMpointer] = syntax.OPCODETOHEXDICT[line[0]]
         self.RAMpointer += 1
-        self.RAM[self.RAMpointer] = line[2] #Operand
-        self.RAMpointer += 1
-
-    def specialOpcode(self, line): #TBF
-        return
 
     def firstPass(self, tokenList): #Being passed a list of tokens for each line (FROM ADI)
         #Using the CIE assembly language from the 9608 specification:
@@ -67,13 +80,19 @@ class Assembler:
         # - Throw the label into the symbol table and keep going until you come across where it's defined
         # - RAMpointer will hold the memory location the label would start at, so we just translate this into hex and throw this into the array
 
-        for item in tokenList: #Iterating through the list to get each line, held as a tuple
-            if len(item) == 3: #Label case
-                self.label(item)
-            elif len(item) == 2: #Opcode Case
-                self.regularOpcode(item)
-            else: #Special case
-                self.specialOpcode(item)
+        for line in tokenList: #Iterating through the list to get each line, held as a tuple
+            if len(line) == 3: #Label case
+                self.label(line)
+            elif len(line) == 2: #Regular opcode Case
+                if line[0] not in syntax.OPCODETOHEXDICT: #Means the first part of the line is a label
+                    self.label(line)
+                else: #Means the line is just a regular opcode
+                    self.regularOpcode(line)
+            else: #Special opcode case
+                self.specialOpcode(line)
+    #self.secondPass()
+
+    #def secondPass(self): #Second pass would probably be needed for "JMP" opcodes, which may jump to an undefined label
 
 
 
@@ -81,7 +100,7 @@ class Assembler:
 if __name__ == "__main__": #Test input for finished functions
     test = Assembler()
     test.init_RAM()
-    test.firstPass([["LABELNAME", "LDD", "7"]])
+    test.firstPass([["JMP", "03"], ["LABELTHING", "END"], ["LABELNAME", "LDD", "7"]])
     test.showContents()
 
 #Consider bus width, might be useful to model
--- CIE Assembler/main.py
+++ CIE Assembler/main.py
@@ -3,24 +3,26 @@ try:
 except:
     from tkinter import *
 from toolbar import *
-from editorButtons import *
+from interpreterControls import *
 from editor import *
-from ramDisplay import *
+from display import *
+from errorLine import *
 
 
 if __name__ == "__main__":
     root = Tk()
     root.title("CIE Assembler virtual machine")
-    root.geometry("1100x600")
+    root.geometry("1500x800")
     root.resizable(False, False)
 
     toolBar = ToolBar(root)
-    editButtons = EditorButtons(root, 0,0)
+    editButtons = InterpreterControls(root, 0,0)
     editor = Editor(root,1,0)
-    ramDisplay = RamDisplay(root, 1,2)
-
-
-
+    display = Display(root, 1,2)
+    errorBar = ErrorBar(root,3,0)
+    editor.rep = errorBar.update
+    root.bind("<F5>", editor.lexical_analysis)
+    toolBar.assign_numSys(display.numSys)
 
 
 
