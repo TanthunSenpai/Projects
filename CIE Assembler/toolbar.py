fcb5300a3cf35316921443a57427acb1638696b6
Kyouma-BAT <kyouma.bat@gmail.com>
2020-05-12 21:57:07 +0600

    *Added display and change of number system using toolbar capability

--- CIE Assembler/toolbar.py
+++ CIE Assembler/toolbar.py
@@ -9,11 +9,21 @@ class ToolBar:
         self.master = master
         self.toolbar = Menu(self.master)
         self.bars = OrderedDict()
+
         self.bars["File"] = Menu(self.toolbar, tearoff = False)
         self.bars["Edit"] = Menu(self.toolbar, tearoff = False)
         self.bars["Tools"] = Menu(self.toolbar, tearoff = False)
         self.bars["View"] = Menu(self.toolbar, tearoff = False)
         self.bars["About"] = Menu(self.toolbar, tearoff = False)
+
+
+        self.numSys = Menu(self.bars["View"], tearoff = False)
+        self.bars["View"].add_cascade(label = "NumberSystem", menu = self.numSys)
+        self.numSys.add_command(label = "Decimal",command =  lambda: self.set_numSys("Dec"))
+        self.numSys.add_command(label = "Hexadecimal", command = lambda: self.set_numSys("Hex"))
+
+
+
 
         for each in self.bars:
             self.toolbar.add_cascade(label = each, menu = self.bars[each])
@@ -24,8 +34,50 @@ class ToolBar:
         self.bars["Edit"].add_command(label = "Preferences")
         self.bars["Tools"].add_command(label = "Symbol Table")
         self.bars["Tools"].add_command(label = "Frequency")
-        self.bars["View"].add_command(label = "Show")
-        self.bars["View"].add_command(label = "Number System")
-        self.bars["About"].add_command(label = "Credits")
-        self.bars["About"].add_command(label = "Version")
-        self.bars["About"].add_command(label = "Documentation")
+        self.bars["About"].add_command(label = "Info")
+
+
+
+
+
+
+
+    def save(self):
+
+        pass
+
+    def pop_load(self):
+        pass
+
+    def set_freq(self):
+
+        pass
+
+    def pop_symbol(self):
+
+        pass
+
+
+
+    def assign_numSys(self,func):
+        self.set_numSys = func
+
+        pass
+
+
+    def pop_symbol(self):
+
+        pass
+
+    def pop_info(self):
+
+        pass
+
+
+if __name__ == "__main__":
+
+    root = Tk()
+    tb = ToolBar(root)
+
+
+    root.mainloop()
