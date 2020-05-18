141a4c2e16fe040d97ab2325d0c54fb13893c2ab
martin lee <zmart.@carelesswhisper.lan>
2020-05-18 19:10:04 +0800

    input and output bars allign now

--- CIE_Assembler/inBar.py
+++ CIE_Assembler/inBar.py
@@ -7,14 +7,14 @@ except:
 class InBar:
     def __init__(self, master, r, c):
         self.master = master
-        self.frame = Frame(master,borderwidth= 5,relief = "groove")
+        self.frame = Frame(master,borderwidth=5,relief="groove")
         self.frame.grid(row=r, column=c)
         self.fontSize = 12
         self.font = ("Consolas",self.fontSize)
-        self.label = Label(self.frame,text="Input:",font=self.font,width=8)
+        self.label = Label(self.frame,text="Input: ",font=self.font,width=5)
         self.label.grid(row=0, column=0)
         self.strVar = StringVar()
-        self.entry = Entry(self.frame,textvariable=self.strVar,width=30,justify=LEFT)
+        self.entry = Entry(self.frame,textvariable=self.strVar,width=29,justify=LEFT)
         self.entry.grid(row=0, column=1)
         self.enterButton = Button(self.frame,text="Enter",font=self.font,width=7,command=self.enterInput)
         self.enterButton.grid(row=0,column=2)
