2cd167a40ab5079c05c0d51df769ef32fc4a6f45
Adi Bozzhanov <kyouma.bat@gmail.com>
2020-05-20 02:02:57 +0600

    *leading new lines

--- CIE_Assembler/editor.py
+++ CIE_Assembler/editor.py
@@ -51,7 +51,7 @@ class Editor:
 
 
     def lexical_analysis(self):
-        text = self.textArea.get('0.0', 'end').strip()
+        text = self.textArea.get('0.0', 'end')
         text = text.split("\n")
         ret = []
         error = False
