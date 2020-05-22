7604dd8f38c461e38a95410c4fe934fb0ba7ee36
martin lee <zmart.@carelesswhisper.lan>
2020-05-22 21:43:33 +0800

    FIXED SYNTAX ERROR

--- CIE_Assembler/syntax.py
+++ CIE_Assembler/syntax.py
@@ -24,26 +24,26 @@ SPECIALOPERANDS = {
 
 
 """
-ram = [ , , ,ldd ,100 , , , , , , , , ]
+"RAM" = [ , , ,ldd ,100 , , , , , , , , ]
              ^
              pc
 !!!
-EVERYTHING IN RAM IS IN HEX
+EVERYTHING IN "RAM" IS IN HEX
 !!!
 
 interpreter:
-    looks at ram[pc]
+    looks at "RAM"["PC"]
     and calls the method
 
 # dictionary that is passed to all functions
 args = {
-    pc - int
-    ram - a list
-    ACC - int
-    IX - int
-    zmp - bool - comparison flag: equal(True), not equal(False)
-    hlt - bool - halt flag
-    err - string - execution result - default: execution successful
+    "PC" - int
+    "RAM" - a list
+    "ACC" - int
+    "IX" - int
+    "ZMP" - bool - comparison flag: equal(True), not equal(False)
+    "halt" - bool - halt flag
+    "errorMsg" - string - execution result - default: execution successful
 }
 
 [   , , , , , inc, 12, , , sto, ,jmp ]
@@ -53,117 +53,117 @@ def stubFunc(args):
     (in ldd case pc+= 2)
     -do stuff
     (in ldd case:)
-    registers["EE"] = ram[int(ram[pc+1], 16)]
+    registers["EE"] = "RAM"[int("RAM"[pc+1], 16)]
 """
 
 def LDM(args):
-    args[ACC] = args[ram][args[pc]+1]
-    args[pc] += 2
+    args["ACC"] = args["RAM"][args["PC"]+1]
+    args["PC"] += 2
 
 def LDD(args):
-    addrss = args[ram][args[pc]+1]
-    args[ACC] = args[ram][addrss]
-    args[pc] += 2
+    addrss = args["RAM"][args["PC"]+1]
+    args["ACC"] = args["RAM"][addrss]
+    args["PC"] += 2
 
 def LDI(args):
-    addrss = args[ram][args[pc]+1]
-    value = args[ram][addrss]
-    args[ACC] = value
-    args[pc] += 2
+    addrss = args["RAM"][args["PC"]+1]
+    value = args["RAM"][addrss]
+    args["ACC"] = value
+    args["PC"] += 2
 
 def LDX(args):
-    addrss = args[ram][args[pc]+1]
-    newAddrss = addrss + args[IX]
-    args[ACC] = args[ram][newAddrss]
-    args[pc] += 2
+    addrss = args["RAM"][args["PC"]+1]
+    newAddrss = addrss + args["IX"]
+    args["ACC"] = args["RAM"][newAddrss]
+    args["PC"] += 2
 
 def LDR(args):
-    args[IX] = args[ram][args[pc]+1]
-    args[pc] += 2
+    args["IX"] = args["RAM"][args["PC"]+1]
+    args["PC"] += 2
 
 def STO(args):
-    args[ram][args[pc]+1] = args[ACC]
-    args[pc] += 2
+    args["RAM"][args["PC"]+1] = args["ACC"]
+    args["PC"] += 2
 
 def ADD(args):
-    args[ACC] += args[ram][args[pc]+1]
-    args[pc] += 2
+    args["ACC"] += args["RAM"][args["PC"]+1]
+    args["PC"] += 2
 
 def INC(args):
-    if args[ram][args[pc]+1] == "EE":
-        args[ACC] += 1
-    elif args[ram][args[pc]+1] == "FF":
-        args[IX] += 1
+    if args["RAM"][args["PC"]+1] == "EE":
+        args["ACC"] += 1
+    elif args["RAM"][args["PC"]+1] == "FF":
+        args["IX"] += 1
     else:
-        args[hlt] = True
-        args[err] = "Register error: operand is not ACC or IX."
-    args[pc] += 2
+        args["halt"] = True
+        args["errorMsg"] = "Register error: operand is not ACC or IX."
+    args["PC"] += 2
 
 def DEC(args):
-    if args[ram][args[pc]+1] == "EE":
-        args[ACC] -= 1
-    elif args[ram][args[pc]+1] == "FF":
-        args[IX] -= 1
+    if args["RAM"][args["PC"]+1] == "EE":
+        args["ACC"] -= 1
+    elif args["RAM"][args["PC"]+1] == "FF":
+        args["IX"] -= 1
     else:
-        args[hlt] = True
-        args[err] = "Register error: only ACC and IX can be decremented."
-    args[pc] += 2
+        args["halt"] = True
+        args["errorMsg"] = "Register error: only ACC and IX can be decremented."
+    args["PC"] += 2
 
 def JMP(args):
-    args[pc] = args[ram][args[pc]+1]
+    args["PC"] = args["RAM"][args["PC"]+1]
 
 def CMP(args):  #format: CMP ADDRESSINGMODE OPERAND
-    args[zmp] = True
-    if args[ram][args[pc]+1] == 0:  #immediate addressing mode
-        num = args[ram][args[pc]+2]
-        if num == args[ACC]:
-            args[zmp] = True
+    args["ZMP"] = True
+    if args["RAM"][args["PC"]+1] == 0:  #immediate addressing mode
+        num = args["RAM"][args["PC"]+2]
+        if num == args["ACC"]:
+            args["ZMP"] = True
         else:
-            args[zmp] = False
+            args["ZMP"] = False
 
-    elif args[ram][args[pc]+1] == 1:   #direct addressing mode
-        addrss = args[ram][args[pc]+2]
-        if args[ram][addrss] == args[ACC]:
-            args[zmp] = True
+    elif args["RAM"][args["PC"]+1] == 1:   #direct addressing mode
+        addrss = args["RAM"][args["PC"]+2]
+        if args["RAM"][addrss] == args["ACC"]:
+            args["ZMP"] = True
         else:
-            args[zmp] = False
+            args["ZMP"] = False
     else:   #neither immediate nor direct addressing modes
-        args[hlt] = True
-        args[err] = "Addressing mode error: only immediate or direct addressing modes are allowed"
-    args[pc] += 3
+        args["halt"] = True
+        args["errorMsg"] = "Addressing mode error: only immediate or direct addressing modes are allowed"
+    args["PC"] += 3
 
 def JPE(args):
-    addrss = args[ram][args[pc]+1]
-    if args[zmp] == True:   #they are equal
-        args[pc] = addrss
+    addrss = args["RAM"][args["PC"]+1]
+    if args["ZMP"] == True:   #they are equal
+        args["PC"] = addrss
     else:   #they are not equal, continue the execution without jumping
-        args[pc] += 2
+        args["PC"] += 2
 
 def JPN(args):
-    addrss = args[ram][args[pc]+1]
-    if args[zmp] == False:  #they are not equal
-        args[pc] = addrss
+    addrss = args["RAM"][args["PC"]+1]
+    if args["ZMP"] == False:  #they are not equal
+        args["PC"] = addrss
     else:   #they are equal, continue the execution without jumping
-        args[pc] += 2
+        args["PC"] += 2
 
 def inStub(chrcter):
     return chrcter
 
 def IN(args):
     chrcter = inStub(chrcter)
-    args[ACC] = ord(chrcter)
-    args[pc] += 1
+    args["ACC"] = ord(chrcter)
+    args["PC"] += 1
 
 def outStub(chrcter):
     pass
 
 def OUT(args):
-    chrcter = chr(args[ACC])
+    chrcter = chr(args["ACC"])
     outStub(chrcter)
-    args[pc] += 1
+    args["PC"] += 1
 
 def END(args):
-    print(args[ACC])
+    print(args["ACC"])
     pass
 
 HEXTOFUNCTIONDICT = {
