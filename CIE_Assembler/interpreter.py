8b916b754103fa14ca94ac0a24f2f7f391aa84a4
Laveen <laveen2512@gmail.com>
2020-05-23 08:34:29 +0100

    Bugs fixed in interpreter.py

--- CIE_Assembler/interpreter.py
+++ CIE_Assembler/interpreter.py
@@ -15,23 +15,28 @@ class Interpreter:
             }
 
     def execute(stepFlag):
-        syntax.HEXTOFUNCTIONDICT[self.args["RAM"][self.args["PC"]]](self.args) #Calls the respective method for the opcode that the PC is pointing at, passing the dictionary in as a parameter
-        if not (self.args["halt"] or stepFlag) or self.args["PC"] > 255: #Checking if we can schedule the next call to execute
-            self.master.after(self.runFreq, lambda: self.execute(stepFlag))
-        return self.args #We return the dictionary for adi to display the error message
+        try:
+            syntax.HEXTOFUNCTIONDICT[self.args["RAM"][self.args["PC"]]](self.args) #Calls the respective method for the opcode that the PC is pointing at, passing the dictionary in as a parameter
+        except:
+            if self.args["RAM"][self.args["PC"]] == "00": #Blank memory location
+                if not self.args["halt"] and not stepFlag: #Checking if we can schedule the next call to execute
+                    self.master.after(self.runFreq, lambda: self.execute(stepFlag))
+                return self.args #We return the dictionary for adi to display the error message
+            else:
+                self.args["RAM"][self.args["halt"]] = True #Setting the halt flag as we have got to an invalid opcode
+                self.args["RAM"][self.args["errorMsg"]] = f"Opcode {self.args["RAM"][self.args["PC"]]} is undefined."
+                return self.args
+        else:
+            if not self.args["halt"] and not stepFlag: #Checking if we can schedule the next call to execute
+                self.master.after(self.runFreq, lambda: self.execute(stepFlag))
+            return self.args #We return the dictionary for adi to display the error message
 
+            
     def updateArgs(): #Stub function which will be overwritten by Adi's updateArgs method
         print(self.args)
 
     def stop(): #Method to be called in the event that the stop button is pressed
         self.args["halt"] = True
-
-    def start(stepFlag): #Method to run code if code has been stopped halfway through. Can also be used if mode is changed from step to run.
-        if self.args["PC"] > 255:
-            self.args["errorMsg"] = "Cannot run code: Pointer has reached end of memory."
-            return self.args
-        else:
-            self.execute(stepFlag)
 
 if __name__ == "__main__": #Debug code
     import assembler
